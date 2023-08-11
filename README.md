9a9abe64e443175f5871fafc69ca598f88cd32b7
parsa-hemmati <89730775+parsa-hemmati@users.noreply.github.com>
2023-08-11 14:45:25 +0000

    Uploaded base files

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # ResearchGPT
+
+ResearchGPT is a Flask-based web application designed to assist medical researchers in performing systematic reviews and meta-analyses. Utilizing OpenAI's GPT-3.5-turbo model, it evaluates the relevance of articles from PubMed based on a user-specified research question.
+
+## Project Structure
+
+\`\`\`
+ResearchGPT/
+├── static/
+│   ├── css/
+│   │   └── main.css
+├── templates/
+│   ├── index.html
+│   └── results.html
+└── app.py
+\`\`\`
+
+## Features
+
+- **PubMed Search**: Allows users to search for relevant articles from PubMed using a custom query.
+- **Relevance Analysis**: Utilizes GPT-3.5-turbo to analyze the relevance of each article based on the research question.
+- **Results Display**: Presents the search results, highlighting relevant articles.
+
+## Installation
+
+1. Clone the repository:
+
+    \`\`\`bash
+    git clone https://github.com/yourusername/ResearchGPT.git
+    \`\`\`
+
+2. Navigate to the project directory:
+
+    \`\`\`bash
+    cd ResearchGPT
+    \`\`\`
+
+3. Install the required dependencies:
+
+    \`\`\`bash
+    pip install -r requirements.txt
+    \`\`\`
+
+## Usage
+
+1. Run the Flask application:
+
+    \`\`\`bash
+    python app.py
+    \`\`\`
+
+2. Open your web browser and navigate to [http://localhost:5000](http://localhost:5000).
+
+3. Enter your email, OpenAI API Key, meta-analysis research question, and PubMed search query.
+
+4. Click on the "Search" button to view the relevant articles.
+
+## Dependencies
+
+- Flask
+- OpenAI
+- Biopython
+- Pandas
+
+Make sure to include a \`requirements.txt\` file with the appropriate version numbers for these packages.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
+
+## Contributing
+
+Feel free to open issues or pull requests if you want to contribute to this project.
+
+## Acknowledgements
+
+Thanks to OpenAI for providing access to the GPT-3.5-turbo model and to the Biopython community for the tools to interact with PubMed.
---
+++ app.py
@@ -0,0 +1,108 @@
+from Bio import Entrez
+from Bio.Medline import parse
+from flask import Flask, render_template, request, redirect, url_for
+import openai
+import pandas as pd
+from io import StringIO
+import csv
+
+app = Flask(__name__)
+
+def fetch_pubmed_articles(query, email):
+    Entrez.email = email
+
+    # Search PubMed
+    search_results = Entrez.esearch(db="pubmed", term=query, retmax=10, sort="relevance")
+    record = Entrez.read(search_results)
+    search_results.close()
+
+    # Get PubMed IDs
+    pubmed_ids = record["IdList"]
+
+    # Fetch details
+    handle = Entrez.efetch(db="pubmed", id=pubmed_ids, rettype="medline", retmode="text")
+    records = parse(handle)
+
+    # Extract information and create DataFrame
+    data = []
+    for rec in records:
+        pmid = rec.get("PMID", "")
+        title = rec.get("TI", "")
+        authors = ", ".join(rec.get("AU", []))
+        journal_citation = rec.get("SO", "")
+        abstract = rec.get("AB", "")
+        abstract_link = f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/"
+        data.append([pmid, title, authors, journal_citation, abstract, abstract_link])
+
+    # Create DataFrame
+    df = pd.DataFrame(data, columns=['PMID', 'Title', 'Authors', 'Journal Citation', 'Abstract', 'Abstract Link'])
+    return df
+
+def is_relevant(article, question, api_key):
+    openai.api_key = api_key
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a medical researching performing a systematic review. You are presented with an article and you have to determine whether the article is related to the research question and should be included in the systematic review and can only reply with relevant or not relevant to this research question: {question}"
+            },
+            {
+                "role": "user",
+                "content": article
+            },
+            {
+                "role": "assistant",
+                "content": ""
+            }
+        ],
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+
+    reply = response.choices[0].message['content']
+    return reply
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        email = request.form['email']
+        api_key = request.form['api_key']
+        question = request.form['question']
+        pubmed_query = request.form['pubmed_query']
+
+        # create a df using PubMed results
+        df = fetch_pubmed_articles(pubmed_query, email)
+
+        # Create a new column for relevancy
+        df['relevancy'] = ""
+
+        # Iterate through the DataFrame and fill the relevancy column
+        for index, row in df.iterrows():
+            article = row['Abstract']
+            relevancy = is_relevant(article, question, api_key)
+            df.at[index, 'relevancy'] = relevancy
+
+        # Convert the DataFrame to CSV and store it in a session variable
+        csv_str = StringIO()
+        df.to_csv(csv_str, index=False)
+        csv_str.seek(0)
+        session['df_csv'] = csv_str.read()
+
+        return redirect(url_for('results'))
+
+    return render_template('index.html')
+
+@app.route('/results', methods=['GET'])
+def results():
+    csv_str = StringIO(session['df_csv'])
+    df = pd.read_csv(csv_str)
+
+    return render_template('results.html', df=df)
+
+if __name__ == '__main__':
+    app.run(debug=True)
